df6a6a3c0ceeff8773470f7189094d874798c4e3
Cornell <32682171+cellerby@users.noreply.github.com>
2018-01-08 10:38:18 -0600

    6_PB_CU5
    The ship can fly and go left and right

---
+++ Assets/Ship.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour {
+
+    Rigidbody rigidBody;
+
+    // Use this for initialization
+    void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) // can trust while rotating
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+
+
+    }
+}
