1b61ad853341d8f514f3905b5c75d161161fc1d6
Cornell <32682171+cellerby@users.noreply.github.com>
2018-04-18 17:00:23 -0500

    Cleaned up folder layout

--- Assets/Osccillator.cs
+++ Assets/Osccillator.cs
@@ -20,7 +20,7 @@ public class Osccillator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
+        if (period <= Mathf.Epsilon) { return; }
         float cycles = Time.time / period; //grows cintunually from 0
 
         const float tau = Mathf.PI * 2f;
